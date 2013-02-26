bd14cee6340649d5422b2b404b5adfb0a054946c
Radu Simion <radu.simion@catalystww.com>
2013-02-26 12:29:56 +0200

    adding comment count

--- wp-content/themes/stylitics/functions.php
+++ wp-content/themes/stylitics/functions.php
@@ -255,6 +255,20 @@ if( class_exists( 'kdMultipleFeaturedImages' ) ) {
         new kdMultipleFeaturedImages( $args4 );
 }
 
+function current_page_url() {
+	$pageURL = 'http';
+	if( isset($_SERVER["HTTPS"]) ) {
+		if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	}
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+	} else {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	return $pageURL;
+}
+
 // limit excerpt in words
 
 function string_limit_words($string, $word_limit) {
