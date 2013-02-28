1cb3851b75fb8938b7ddeca4fb4a2904b7ce6f7c
Radu Simion <radu.simion@catalystww.com>
2013-02-28 20:53:25 +0200

    fixes2

--- wp-content/plugins/wp-featured-content-slider/content-slider.php
+++ wp-content/plugins/wp-featured-content-slider/content-slider.php
@@ -52,10 +52,6 @@ $c_slider_class = c_slider_get_dynamic_class();
 				}
 			?>
             <div class="post-content">
-                <div class="time">
-                    <span class="month"><?php the_time('F'); ?></span> <br/>
-                    <span class="day"><?php the_time('j'); ?><sup><?php the_time('S'); ?></sup></span>
-                </div>
                 <h2 class="title">
                     <a href="<?php the_permalink();?>"><?php the_title();?></a>
                 </h2>
